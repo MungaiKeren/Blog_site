9405def78bffa4fc906cf902aa5afb1f014dd6e8
MungaiKeren <wambukeren@gmail.com>
2019-09-23 09:00:00 +0300

    This commit displays the user profile picture uploads set

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Config:
     # QUOTES_API_BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://keren:kayren@localhost/bloggy'
     SECRET_KEY = 'cd48e1c22de0961d5d1bfb14f8a66e006cfb1cfbf3f0c0f3'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
     @staticmethod
     def init_app(app):
